f338b554cd197198593ea6cbe8e0fd54bcbc9d7c
Paweł Wegner <pawel.wegner95@gmail.com>
2021-04-18 10:15:58 +0200

    file_utils: added.

---
+++ src/coro/cloudstorage/util/file_utils.h
@@ -0,0 +1,32 @@
+#ifndef CORO_CLOUDSTORAGE_FUSE_FILE_UTILS_H
+#define CORO_CLOUDSTORAGE_FUSE_FILE_UTILS_H
+
+#include <coro/generator.h>
+#include <coro/util/thread_pool.h>
+
+#include <cstdio>
+
+namespace coro::cloudstorage::util {
+
+struct FileDeleter {
+  void operator()(std::FILE* file) const {
+    if (file) {
+      fclose(file);
+    }
+  }
+};
+
+int64_t Ftell(std::FILE* file);
+int Fseek(std::FILE* file, int64_t offset, int origin);
+std::unique_ptr<std::FILE, FileDeleter> CreateTmpFile();
+Task<int64_t> GetFileSize(coro::util::ThreadPool* thread_pool, std::FILE* file);
+Task<> WriteFile(coro::util::ThreadPool* thread_pool, std::FILE* file,
+                 int64_t offset, std::string_view data);
+Generator<std::string> ReadFile(coro::util::ThreadPool* thread_pool,
+                                std::FILE* file);
+Task<std::string> ReadFile(coro::util::ThreadPool* thread_pool, std::FILE* file,
+                           int64_t offset, size_t size);
+
+}  // namespace coro::cloudstorage::util
+
+#endif  // CORO_CLOUDSTORAGE_FUSE_FILE_UTILS_H
---
+++ src/coro/cloudstorage/util/string_utils.h
@@ -0,0 +1,49 @@
+#ifndef CORO_CLOUDSTORAGE_UTIL_STRING_UTILS_H
+#define CORO_CLOUDSTORAGE_UTIL_STRING_UTILS_H
+
+#include <string>
+#include <string_view>
+
+namespace coro::cloudstorage::util {
+
+template <typename T>
+std::string ToString(T d) {
+  std::stringstream stream;
+  stream << std::move(d);
+  return std::move(stream).str();
+}
+
+inline std::string ToString(std::string_view sv) { return std::string(sv); }
+
+inline std::string StrCat() { return ""; }
+
+template <typename Head, typename... Tail>
+std::string StrCat(Head&& head, Tail&&... tail) {
+  std::string result = ToString(std::forward<Head>(head));
+  result += StrCat(std::forward<Tail>(tail)...);
+  return result;
+}
+
+template <typename T, typename C>
+std::vector<T> SplitString(const T& string, C delim) {
+  std::vector<T> result;
+  T current;
+  for (auto c : string) {
+    if (c == delim) {
+      if (!current.empty()) {
+        result.emplace_back(std::move(current));
+      }
+      current.clear();
+    } else {
+      current += c;
+    }
+  }
+  if (!current.empty()) {
+    result.emplace_back(std::move(current));
+  }
+  return result;
+}
+
+}  // namespace coro::cloudstorage::util
+
+#endif  // CORO_CLOUDSTORAGE_FUSE_STRING_UTILS_H
