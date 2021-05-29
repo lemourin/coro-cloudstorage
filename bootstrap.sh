1eae2986717d485747d099d83ba4f8e829d46c87
Paweł Wegner <pawel.wegner95@gmail.com>
2021-05-29 17:42:22 +0200

    contrib: update cryptopp.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,5 @@ cd $(dirname $0)
 
 git submodule update --init --checkout --recursive -f
 
-curl https://raw.githubusercontent.com/noloader/cryptopp-cmake/CRYPTOPP_8_2_0/CMakeLists.txt \
-  -s -o contrib/cryptopp/CMakeLists.txt
-sed -i 's/\\\"winapifamily\.h\\\"/winapifamily\.h/g' contrib/cryptopp/CMakeLists.txt 
-curl https://raw.githubusercontent.com/noloader/cryptopp-cmake/master/cryptopp-config.cmake \
-  -s -o contrib/cryptopp/cryptopp-config.cmake
-
 ./contrib/coro-http/bootstrap.sh
 
