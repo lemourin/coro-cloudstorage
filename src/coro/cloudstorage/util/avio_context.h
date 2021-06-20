d451eac62e464470b557ef4bd16e9618a472bd3d
Paweł Wegner <pawel.wegner95@gmail.com>
2021-06-21 00:46:35 +0200

    avio_context: check interrupt state.

--- src/coro/cloudstorage/util/avio_context.h
+++ src/coro/cloudstorage/util/avio_context.h
@@ -4,6 +4,7 @@
 #include <coro/cloudstorage/cloud_provider.h>
 #include <coro/util/event_loop.h>
 
+#include <cerrno>
 #include <future>
 
 extern "C" {
@@ -34,7 +35,7 @@ auto CreateIOContext(EventLoop* event_loop, CloudProvider* provider, File file,
   };
   const int kBufferSize = 4 * 1024;
   auto buffer = static_cast<uint8_t*>(av_malloc(kBufferSize));
-  return std::unique_ptr<AVIOContext, AVIOContextDeleter>(avio_alloc_context(
+  std::unique_ptr<AVIOContext, AVIOContextDeleter> context(avio_alloc_context(
       buffer, kBufferSize, /*write_flag=*/0,
       new Context{event_loop, provider, std::move(file), 0,
                   std::move(stop_token)},
@@ -45,6 +46,9 @@ auto CreateIOContext(EventLoop* event_loop, CloudProvider* provider, File file,
           try {
             if (data->offset == CloudProvider::GetSize(data->file)) {
               co_return promise.set_value(AVERROR_EOF);
+            }
+            if (data->stop_token.stop_requested()) {
+              co_return promise.set_value(AVERROR(EINTR));
             }
             if (!data->generator) {
               data->generator = data->provider->GetFileContent(
@@ -70,33 +74,43 @@ auto CreateIOContext(EventLoop* event_loop, CloudProvider* provider, File file,
         if (whence == AVSEEK_SIZE) {
           return CloudProvider::GetSize(data->file).value_or(-1);
         }
+        int64_t new_offset = -1;
         if (whence == SEEK_SET) {
-          data->offset = offset;
+          new_offset = offset;
         } else if (whence == SEEK_CUR) {
-          data->offset += offset;
+          new_offset = data->offset + offset;
         } else if (whence == SEEK_END) {
           auto size = CloudProvider::GetSize(data->file);
           if (!size) {
             return -1;
           }
-          data->offset = *size + offset;
+          new_offset = *size + offset;
         } else {
           return -1;
+        }
+        if (data->offset == new_offset) {
+          return new_offset;
         }
         std::promise<int64_t> promise;
         data->event_loop->RunOnEventLoop([&]() -> Task<> {
           try {
+            if (data->stop_token.stop_requested()) {
+              co_return promise.set_value(-1);
+            }
             data->generator = data->provider->GetFileContent(
-                data->file, http::Range{.start = data->offset},
-                data->stop_token);
+                data->file, http::Range{.start = new_offset}, data->stop_token);
             data->it = co_await data->generator->begin();
-            promise.set_value(data->offset);
+            promise.set_value(new_offset);
           } catch (...) {
             promise.set_value(-1);
           }
         });
-        return promise.get_future().get();
+        return data->offset = promise.get_future().get();
       }));
+  if (!context) {
+    throw std::runtime_error("avio_alloc_context");
+  }
+  return context;
 }
 
 }  // namespace coro::cloudstorage::util
