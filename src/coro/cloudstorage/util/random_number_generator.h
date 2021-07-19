613032e23b35c5c44015e70e5981f2727c7373a8
Paweł Wegner <pawel.wegner95@gmail.com>
2021-07-19 23:05:04 +0200

    random_number_generator: handle one byte types.

--- src/coro/cloudstorage/util/random_number_generator.h
+++ src/coro/cloudstorage/util/random_number_generator.h
@@ -12,8 +12,11 @@ class RandomNumberGenerator {
 
   template <typename T>
   T Get() {
-    std::uniform_int_distribution<T> dist;
-    return dist(*engine_);
+    std::uniform_int_distribution<std::conditional_t<
+        std::is_same_v<uint8_t, T>, uint32_t,
+        std::conditional_t<std::is_same_v<int8_t, T>, int32_t, T>>>
+        dist;
+    return T(dist(*engine_));
   }
 
  private:
