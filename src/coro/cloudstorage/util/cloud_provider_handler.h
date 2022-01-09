30d45bd87f84ad0738385883e8d9a7dfc131e5c7
Paweł Wegner <pawel.wegner95@gmail.com>
2022-01-09 12:18:49 +0100

    cloud_provider_handler: try to guess host ip when not available.

--- src/coro/cloudstorage/util/cloud_provider_handler.h
+++ src/coro/cloudstorage/util/cloud_provider_handler.h
@@ -81,23 +81,53 @@ class CloudProviderHandler {
   static std::string GetPathPrefix(
       std::span<const std::pair<std::string, std::string>> headers) {
     namespace re = coro::util::re;
-    if (auto host = http::GetCookie(headers, "host")) {
-      auto host_addresses = GetHostAddresses();
-      if (std::find(host_addresses.begin(), host_addresses.end(), *host) ==
-          host_addresses.end()) {
+    std::optional<std::vector<std::string>> host_addresses;
+    auto host = [&]() -> std::string {
+      auto host = http::GetCookie(headers, "host");
+      if (!host || host->empty()) {
         return "";
       }
+      host_addresses = GetHostAddresses();
+      if (std::find(host_addresses->begin(), host_addresses->end(), *host) ==
+          host_addresses->end()) {
+        return "";
+      }
+      return *host;
+    }();
+    if (host.empty()) {
+      if (!host_addresses) {
+        host_addresses = GetHostAddresses();
+      }
+      std::optional<int> idx;
+      bool ambiguous = false;
+      for (size_t i = 0; i < host_addresses->size(); i++) {
+        if ((*host_addresses)[i] != "127.0.0.1") {
+          if (idx) {
+            ambiguous = true;
+            break;
+          }
+          idx = i;
+        }
+      }
+      if (!ambiguous && idx) {
+        host = std::move((*host_addresses)[*idx]);
+      }
+    }
+    if (host.empty()) {
+      return "";
+    }
+    auto port = [&]() -> std::string_view {
       re::regex regex(R"((\:\d{1,5})$)");
       re::match_results<std::string::const_iterator> match;
       std::string_view port = "";
       if (re::regex_search(http::GetHeader(headers, "host").value(), match,
                            regex)) {
-        port = std::string_view(&*match[1].begin(), match[1].length());
+        return std::string_view(&*match[1].begin(), match[1].length());
+      } else {
+        return "";
       }
-      return StrCat("http://", *host, port);
-    } else {
-      return "";
-    }
+    }();
+    return StrCat("http://", host, port);
   }
 
   static Generator<std::string> GetDashPlayer(std::string path) {
