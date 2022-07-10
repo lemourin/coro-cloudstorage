94d57cb0517a8853290a9cd77e063dc0f73493db
Paweł Wegner <pawel.wegner95@gmail.com>
2022-07-10 14:35:16 +0200

    fetch_json: check for 401 code.

--- src/coro/cloudstorage/util/fetch_json.h
+++ src/coro/cloudstorage/util/fetch_json.h
@@ -5,6 +5,7 @@
 
 #include "coro/http/http.h"
 #include "coro/http/http_parse.h"
+#include "coro/cloudstorage/cloud_exception.h"
 
 namespace coro::cloudstorage::util {
 
@@ -17,7 +18,9 @@ Task<nlohmann::json> FetchJson(const http::Http& http, RequestType request,
   auto response =
       co_await http.Fetch(std::move(request), std::move(stop_token));
   std::string body = co_await http::GetBody(std::move(response.body));
-  if (response.status / 100 != 2) {
+  if (response.status == 401) {
+    throw CloudException(CloudException::Type::kUnauthorized);
+  } else if (response.status / 100 != 2) {
     throw coro::http::HttpException(response.status, std::move(body));
   }
   co_return nlohmann::json::parse(std::move(body));
