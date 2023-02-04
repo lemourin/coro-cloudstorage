dadede3c9e585d00adfc2537ab47ce8c77d8b943
Paweł Wegner <pawel.wegner95@gmail.com>
2023-02-04 14:49:40 +0100

    cloud_factory_config: fix default post auth redirect uri.

--- src/coro/cloudstorage/util/cloud_factory_config.cc
+++ src/coro/cloudstorage/util/cloud_factory_config.cc
@@ -7,7 +7,7 @@ namespace coro::cloudstorage::util {
 
 std::string CloudFactoryConfig::GetDefaultPostAuthRedirectUri(
     std::string_view account_type, std::string_view username) {
-  return StrCat("/", account_type, "/", http::EncodeUri(username));
+  return StrCat("/list/", account_type, "/", http::EncodeUri(username));
 };
 
 }  // namespace coro::cloudstorage::util
