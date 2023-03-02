4b0c99c69805f4b2186dfecd6267cd90e9d15742
Paweł Wegner <pawel.wegner95@gmail.com>
2023-03-03 00:03:24 +0100

    cloud_factory_config: make it copyable.

--- src/coro/cloudstorage/util/cloud_factory_config.h
+++ src/coro/cloudstorage/util/cloud_factory_config.h
@@ -7,15 +7,14 @@
 #include "coro/cloudstorage/util/auth_data.h"
 #include "coro/cloudstorage/util/settings_utils.h"
 #include "coro/http/cache_http.h"
-#include "coro/stdx/any_invocable.h"
 
 namespace coro::cloudstorage::util {
 
 struct CloudFactoryConfig {
   coro::http::CacheHttpConfig http_cache_config = {};
   std::string config_path = GetConfigFilePath();
-  stdx::any_invocable<std::string(std::string_view account_type,
-                                  std::string_view username) const>
+  std::function<std::string(std::string_view account_type,
+                            std::string_view username)>
       post_auth_redirect_uri = GetDefaultPostAuthRedirectUri;
   AuthData auth_data = GetDefaultAuthData();
 
