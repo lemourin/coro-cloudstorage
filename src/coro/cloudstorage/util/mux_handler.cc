1a5c23551fbcb85a34c407658667efe0163a8a1b
Paweł Wegner <pawel.wegner95@gmail.com>
2023-04-17 12:10:26 +0200

    mux_handle: provide sample data.

--- src/coro/cloudstorage/util/mux_handler.cc
+++ src/coro/cloudstorage/util/mux_handler.cc
@@ -17,6 +17,11 @@ namespace {
 
 using ::coro::util::MakeUniqueStopTokenOr;
 
+constexpr const char* kMp4Sample =
+    R"(/mux/?video_account_type=mega&audio_account_type=mega&video_account_name=lemourin%40gmail.com&audio_account_name=lemourin%40gmail.com&video_path=/I%E2%80%99m%20Your%20Treasure%20Box%20%EF%BC%8A%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AF%20%E3%83%9E%E3%83%AA%E3%83%B3%E3%81%9B%E3%82%93%E3%81%A1%E3%82%87%E3%81%86%E3%82%92%20%E3%81%9F%E3%81%8B%E3%82%89%E3%81%B0%E3%81%93%E3%81%8B%E3%82%89%E3%81%BF%E3%81%A4%E3%81%91%E3%81%9F/%E3%80%90original%20anime%20MV%E3%80%91I%E2%80%99m%20Your%20Treasure%20Box%20%EF%BC%8A%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AF%20%E3%83%9E%E3%83%AA%E3%83%B3%E3%81%9B%E3%82%93%E3%81%A1%E3%82%87%E3%81%86%E3%82%92%20%E3%81%9F%E3%81%8B%E3%82%89%E3%81%B0%E3%81%93%E3%81%8B%E3%82%89%E3%81%BF%E3%81%A4%E3%81%91%E3%81%9F%E3%80%82%E3%80%90hololive%E2%A7%B8%E5%AE%9D%E9%90%98%E3%83%9E%E3%83%AA%E3%83%B3%E3%80%91%20%5BvV-5W7SFHDc%5D.mp4&audio_path=/I%E2%80%99m%20Your%20Treasure%20Box%20%EF%BC%8A%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AF%20%E3%83%9E%E3%83%AA%E3%83%B3%E3%81%9B%E3%82%93%E3%81%A1%E3%82%87%E3%81%86%E3%82%92%20%E3%81%9F%E3%81%8B%E3%82%89%E3%81%B0%E3%81%93%E3%81%8B%E3%82%89%E3%81%BF%E3%81%A4%E3%81%91%E3%81%9F/%E3%80%90original%20anime%20MV%E3%80%91I%E2%80%99m%20Your%20Treasure%20Box%20%EF%BC%8A%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AF%20%E3%83%9E%E3%83%AA%E3%83%B3%E3%81%9B%E3%82%93%E3%81%A1%E3%82%87%E3%81%86%E3%82%92%20%E3%81%9F%E3%81%8B%E3%82%89%E3%81%B0%E3%81%93%E3%81%8B%E3%82%89%E3%81%BF%E3%81%A4%E3%81%91%E3%81%9F%E3%80%82%E3%80%90hololive%E2%A7%B8%E5%AE%9D%E9%90%98%E3%83%9E%E3%83%AA%E3%83%B3%E3%80%91%20%5BvV-5W7SFHDc%5D.m4a&format=mp4)";
+constexpr const char* kWebmSample =
+    R"(/mux/?video_account_type=mega&audio_account_type=mega&video_account_name=lemourin%40gmail.com&audio_account_name=lemourin%40gmail.com&video_path=/Imagine%20Dragons%20%26%20JID%20-%20Enemy%20%28from%20the%20series%20Arcane%EF%BC%9A%20League%20of%20Legends%29%20%EF%BD%9C%20Official%20Music%20Video/Imagine%20Dragons%20%26%20JID%20-%20Enemy%20%28from%20the%20series%20Arcane%EF%BC%9A%20League%20of%20Legends%29%20%EF%BD%9C%20Official%20Music%20Video%20%5BF5tSoaJ93ac%5D.webm&audio_path=/Imagine%20Dragons%20%26%20JID%20-%20Enemy%20%28from%20the%20series%20Arcane%EF%BC%9A%20League%20of%20Legends%29%20%EF%BD%9C%20Official%20Music%20Video/Imagine%20Dragons%20%26%20JID%20-%20Enemy%20%28from%20the%20series%20Arcane%EF%BC%9A%20League%20of%20Legends%29%20%EF%BD%9C%20Official%20Music%20Video%20%5BF5tSoaJ93ac%5D.audio.webm&format=webm)";
+
 std::vector<std::string> GetPathComponents(const std::string& encoded_path) {
   std::vector<std::string> components = SplitString(encoded_path, '/');
   for (auto& component : components) {
@@ -41,6 +46,14 @@ std::shared_ptr<CloudProviderAccount> FindAccount(
 Task<http::Response<>> MuxHandler::operator()(
     http::Request<> request, stdx::stop_token stop_token) const {
   http::Uri uri = http::ParseUri(request.url);
+  if (uri.path == "/mux/sample.mp4") {
+    co_return http::Response<>{.status = 302,
+                               .headers = {{"Location", kMp4Sample}}};
+  }
+  if (uri.path == "/mux/sample.webm") {
+    co_return http::Response<>{.status = 302,
+                               .headers = {{"Location", kWebmSample}}};
+  }
   if (!uri.query) {
     co_return http::Response<>{.status = 400};
   }
