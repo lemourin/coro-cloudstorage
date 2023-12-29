27162278495ee605a057b18ada899395b838ee47
Paweł Wegner <pawel.wegner95@gmail.com>
2023-12-29 22:31:40 +0100

    test: add exif orientation test.

--- test/functional_test.cc
+++ test/functional_test.cc
@@ -717,5 +717,54 @@ TEST_F(FunctionalTest, MuxerTest) {
   EXPECT_EQ(response.body, GetTestFileContent("muxed.mp4"));
 }
 
+TEST_F(FunctionalTest, ThumbnailGeneratorRespectsExifOrientation) {
+  FakeHttpClient http;
+  http.Expect(HttpRequest("https://accounts.google.com/o/oauth2/token")
+                  .WillReturn(R"js({
+                    "access_token": "access_token",
+                    "refresh_token": "refresh_token"
+                  })js"))
+      .Expect(HttpRequest("https://www.googleapis.com/drive/v3/"
+                          "about?fields=user,storageQuota")
+                  .WillReturn(R"js({
+                    "user": {
+                      "emailAddress": "test@gmail.com"
+                    },
+                    "storageQuota": {
+                      "usage": "2137"
+                    }
+                  })js"))
+      .Expect(
+          HttpRequest(
+              fmt::format("https://www.googleapis.com/drive/v3/files/id1?{}",
+                          http::FormDataToString(
+                              {{"fields",
+                                "id,name,thumbnailLink,trashed,mimeType,"
+                                "iconLink,parents,size,modifiedTime"}})))
+              .WillReturn(R"js({
+                "id": "id1",
+                "name": "frame.jpg",
+                "thumbnailLink": "thumbnail-link",
+                "modifiedTime": "2023-12-29T12:29:03Z",
+                "parents": [ "root" ],
+                "size": "9447",
+                "mimeType": "image/jpeg"
+              })js"))
+      .Expect(HttpRequest("thumbnail-link")
+                  .WillReturn(ResponseContent{.status = 404}))
+      .Expect(
+          HttpRequest("https://www.googleapis.com/drive/v3/files/id1?alt=media")
+              .WillRespondToRangeRequestWith(GetTestFileContent("frame-exif.jpg")));
+  TestHelper test_helper(std::move(http));
+  ASSERT_EQ(test_helper.Fetch({.url = "/auth/google?code=test"}).status, 302);
+
+  auto account = test_helper.GetAccount(
+      CloudProviderAccount::Id{.type = "google", .username = "test@gmail.com"});
+  auto response =
+      test_helper.Fetch({.url = "/thumbnail/google/test%40gmail.com/id1"});
+  EXPECT_EQ(response.status, 200);
+  EXPECT_EQ(response.body, GetTestFileContent("thumbnail-exif.png"));
+}
+
 }  // namespace
 }  // namespace coro::cloudstorage
